45df9d19e068daf8b9cd1cd2ebb77a363f03e909
Hamza Daoud <hamzadaoud99@gmail.com>
2022-01-16 14:44:08 +0200

    Implementing all function

--- functions.py
+++ functions.py
@@ -1,11 +1,47 @@
-def handel_video(url):
-    pass
+import os
+from pytube import YouTube
+from pytube import Playlist
+import shutil
 
-def handel_video(video):
-    pass
+def create_folder(path):
+    pathToCheck = "./Output/{}".format(path)
+    if(not os.path.isdir(pathToCheck)):
+        os.makedirs(pathToCheck)
+        print("[create_folder] New folder is created!")
 
-def handel_playlist(url):
-    pass
+def handel_url(url):
+    try:
+        result = YouTube(url)
+        return result
+    except Exception as e:
+        print("[handel_url] Something went wrong!")
+        print(str(e))
 
-def create_folder():
-    pass
+def handel_video(video,format,path):
+    try:
+        title = video.title
+        print("[handel_video] '{}' downloading".format(title))
+        oldPath = ''
+        if(format == "audio"):
+            oldPath = video.streams.get_audio_only().download()
+        else:
+            oldPath = video.streams.get_highest_resolution().download()
+        title = oldPath.split("\\")[-1]
+        newPath = "./Output/{}/{}".format(path,title)
+        shutil.move(oldPath,newPath)
+        print("[handel_video] '{}' downloaded and moved to the path".format(title))
+    except Exception as e:
+        print("[handel_video] Something went wrong!")
+        print(str(e))
+
+def handel_playlist(url,format,path):
+    try:
+        playlist = Playlist(url)
+        path = "{}/{}".format(path,playlist.title)
+        create_folder(path)
+        print("[handel_playlist] '{}' downloading".format(playlist.title))
+        for video in playlist.videos:
+            handel_video(video,format,path)
+    except Exception as e:
+        print("[handel_playlist] Something went wrong!")
+        print(str(e))
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from pytube import YouTube
 import csv
+import functions
 file = open("Videos.csv","r")
 csv_reader = csv.reader(file,delimiter=',')
 counter = 0
@@ -8,11 +8,14 @@ for row in csv_reader:
     if(counter == 0):
         counter = 1
     else:
-        print(row)
-
+        url = row[0]
+        playlist = row[1]
+        format = row[2]
+        path = row[3]
+        functions.create_folder(path)
+        if(playlist == "1"):
+            functions.handel_playlist(url,format,path)
+        else:
+            video = functions.handel_url(url)
+            functions.handel_video(video,format,path)
 file.close()
-
-
-video = YouTube('https://www.youtube.com/watch?v=cAybOj9hQe8')
-
-#print(video.streams.get_audio_only().download())
